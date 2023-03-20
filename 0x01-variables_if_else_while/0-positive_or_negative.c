bb85a28bc6279d50fb2ad67782b7c7b61d5dce8a
siombekimatia <siombekimatia@gmail.com>
2023-03-20 11:16:32 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,8 +19,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		else
-			printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
